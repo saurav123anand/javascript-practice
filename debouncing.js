dc1ae491551cc981b9d55d988f09ecd7cec47570
saurav123anand <123sauravanand@gmail.com>
2023-12-09 02:27:01 +0530

    debouncing added

---
+++ debouncing.js
@@ -0,0 +1,21 @@
+
+// it is used to reduce the number of calls and achieved by setTimeOut
+let counter=0;
+function getData(i){
+    console.log("fetching data..."+counter++)
+}
+
+let i=0;
+function myDebounce(callback,delay){
+    let timer;
+    return function(){
+        if(timer){
+            clearTimeout(timer);
+        }
+        setTimeout(() => {
+            callback(i++)
+        }, delay);
+    }
+}
+
+const betterFunction=myDebounce(getData,1000)

03a485d7d3e59f1301cfea5da051babf06543d1c
saurav123anand <123sauravanand@gmail.com>
2023-12-09 19:40:12 +0530

    callback hell added

---
+++ callbackhell.js
@@ -0,0 +1,4 @@
+// issues with callbacks
+// 1. callback hell  --> callback inside callback and so on...
+// 2. Inversion of control--> we are giving control to other function to call our function as a callback but what if the function 
+// can't call the callback due to some issue then 

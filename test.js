aef35c1792ad19298f853beb14e8f66cf406da3e
saurav123anand <123sauravanand@gmail.com>
2023-12-29 19:39:30 +0530

    test file added

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
-let obj1={
-    id:1,
-    name:"scooby"
+func()
+function func(){
+    var a=9;
+    console.log(a);
 }
-let obj2=obj1;
-obj2.name="scooby2";
-console.log(obj1);
-console.log(obj2);
